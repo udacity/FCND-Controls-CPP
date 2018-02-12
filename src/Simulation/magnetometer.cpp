d0496243e3a136a77da808a38fba64aa7962257d
harshitk95 <hkhurana@student.ethz.ch>
2018-02-12 17:27:11 +0100

    Changed the y magnetic field value in Magnetic north frame
    Conform to the PX4 values used (y = 0, in magnetic north frame)

--- src/Simulation/magnetometer.cpp
+++ src/Simulation/magnetometer.cpp
@@ -1,7 +1,7 @@
 #include "magnetometer.h"
 
 void magnetometer::magnetometer_sensor(float declination, SLR::Quaternion<float> attitude, V3F &mag_measurement){
-  mag = V3F(0.215212f,0.002806f,-0.42741f); // Values taken from WMM 2015, NED Frame, order 10^5 nT // maybe a recheck
+  mag = V3F(0.215212f,0.0f,-0.42741f); // Values taken from WMM 2015, NED Frame, order 10^5 nT // maybe a recheck
   SLR::Quaternion<float> declination_quat;
   declination_quat = SLR::Quaternion<float>::FromEulerYPR(-declination,0.0f,0.0f);
   mag_measurement = declination_quat.Rotate_ItoB(mag);
