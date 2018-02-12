e90bd4e5ccb417f188c6a7b22c87196b7e1158f5
harshitk95 <hkhurana@student.ethz.ch>
2018-02-12 15:27:10 +0100

    Adds magnetometer simulation.
    Asks for declination angle as input.
    Values for magnetic field being used are for Zurich acc to WMM 2015
    Can be changed to another place in magnetometer.cpp

---
+++ src/Simulation/magnetometer.cpp
@@ -0,0 +1,25 @@
+#include "magnetometer.h"
+
+void magnetometer::magnetometer_sensor(float declination, V3F &mag_measurement){
+  mag = V3F(0.215212f,0.002806f,-0.42741f); // Values taken from SITLGazebo PX4 (WMM 2015) NED Frame, order 10^5 nT
+  SLR::Quaternion<float> declination_quat;
+  declination_quat = SLR::Quaternion<float>::FromEulerYPR(-declination,0.0f,0.0f);
+  mag_measurement = declination_quat.Rotate_ItoB(mag);
+
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+
+  std::normal_distribution<> fx{0,fx_stddev};
+  std::normal_distribution<> fy{0,fy_stddev};
+  std::normal_distribution<> fz{0,fz_stddev};
+
+  float fx_sample = fx(gen);
+  float fy_sample = fy(gen);
+  float fz_sample = fz(gen);
+
+  mag_measurement.x += fx_sample;
+  mag_measurement.y += fy_sample;
+  mag_measurement.z += fz_sample;
+
+}
---
+++ src/Simulation/magnetometer.h
@@ -0,0 +1,21 @@
+#ifndef MAGNETOMETER_H
+#define MAGNETOMETER_H
+
+#include <math.h>
+#include "Common.h"
+#include "Math/Random.h"
+#include "Math/Quaternion.h"
+#include "matrix/math.hpp"
+#include <random>
+
+class magnetometer
+{
+public:
+  V3F mag;
+  float fx_stddev = 0.0001f;
+  float fy_stddev = 0.0001f;
+  float fz_stddev = 0.0001f;
+  void magnetometer_sensor(float declination, V3F &mag_measurement);
+};
+
+#endif // MAGNETOMETER_H
